3e4a153d1e59b716dc61f08de4fed5179c591cb8
Zhibolife <1044451103@qq.com>
2018-09-16 19:24:45 +0800

    地形拖拽凹陷和恢复

--- test0911/Assets/Script/MouseDrag.cs
+++ test0911/Assets/Script/MouseDrag.cs
@@ -13,13 +13,30 @@ public class MouseDrag : MonoBehaviour
     RaycastHit posPoint;
     //设置地面层，我的地面层是第8层，所以是8。
     LayerMask mask = 1 << 8;
+
+    //是否碰到了地形
+    bool isColliderTerr;
+
+    //初始heightMap 用于还原地形
+    float[,] heightMapBackup;
+
+
     void Start()
     {
+        heightMapBackup = ter.terrainData.GetHeights(0, 0, ter.terrainData.heightmapWidth, ter.terrainData.heightmapHeight);
     }
     void Update()
     {
         clickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+    }
+
+
+    void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("碰撞--" + collision.gameObject.name);
+        if (collision.gameObject == ter.gameObject)
+            isColliderTerr = true;
     }
     void OnMouseDown()
     {
@@ -32,34 +49,53 @@ public class MouseDrag : MonoBehaviour
 
     }
 
-    float r = 1.5f;
+
+    void OnDestroy()
+    {
+        Debug.Log("销毁还原");
+        ReSet();
+    }
+
+    public void ReSet()
+    {
+        ter.terrainData.SetHeights(0, 0, heightMapBackup);
+
+
+    }
+    float r;
 
     void OnMouseDrag()
     {
-
         //取射线与地面相碰的坐标，赋给mouseMove，再把mouseMove的X坐标和Z坐标赋给物体，
         //Y坐标不变（因为是贴在地面移动）
+        r = transform.GetComponent<SphereCollider>().radius;
+        //  Debug.Log(transform.GetComponent<SphereCollider>().radius);
         Physics.Raycast(clickRay, out posPoint, Mathf.Infinity, mask.value);
         Vector3 mouseMove = posPoint.point;
         transform.position = (new Vector3(mouseMove.x, transform.position.y, mouseMove.z));
-        Vector3 terrainLocalPos = transform.position - ter.transform.position;  //物件在地形上的相对位置
-        Vector2 controlPos = new Vector2(terrainLocalPos.x / ter.terrainData.size.x * ter.terrainData.heightmapWidth, terrainLocalPos.z / ter.terrainData.size.z * ter.terrainData.heightmapHeight);
-        for (int x1 = 0; x1 < ter.terrainData.size.x; x1++)   //遍历所有点
+        if (isColliderTerr)
         {
-            for (int z1 = 0; z1 < ter.terrainData.size.z; z1++)
+            Vector3 terrainLocalPos = transform.position - ter.transform.position;  //物件在地形上的相对位置
+            Vector2 controlPos = new Vector2(terrainLocalPos.x / ter.terrainData.size.x * ter.terrainData.heightmapWidth, terrainLocalPos.z / ter.terrainData.size.z * ter.terrainData.heightmapHeight);
+            Debug.LogError(ter.terrainData.size + "  " + ter.terrainData.heightmapScale.y);
+            for (int x1 = 0; x1 < ter.terrainData.size.x; x1++)   //遍历所有点
             {
-
-                if (Mathf.Abs(x1 - controlPos.x) <= r && Mathf.Abs(z1 - controlPos.y) <= r)  //首先缩小范围
+                for (int z1 = 0; z1 < ter.terrainData.size.z; z1++)
                 {
-                    float powDis = Mathf.Pow((x1 - controlPos.x), 2) - Mathf.Pow((z1 - controlPos.y), 2); ///当前点与原点的距离的平方
-                    float powr = Mathf.Pow(r, 2);
-                    if (powDis <= powr)    ///再次缩小范围if (Vector3.Distance(transform.position, new Vector3(x1, 6, z1)) < r)   //Vector3.Distance(transform.position, new Vector3(x1, 6, z1)) 
+
+                    if (Mathf.Abs(x1 - controlPos.x) <= r && Mathf.Abs(z1 - controlPos.y) <= r)  //首先缩小范围
                     {
-                        float sinkHeight; //要降低的高度
-                        float oldHeight = ter.terrainData.GetHeight(x1, z1);//输出的是介于0到1之间的高度
-                        sinkHeight = Mathf.Sqrt(powr - powDis);
-                        float[,] newHeightData = new float[1, 1] { { (oldHeight - sinkHeight) / ter.terrainData.heightmapScale.y } };  //新的高度值
-                        ter.terrainData.SetHeights(x1, z1, newHeightData);  //设定新的高度值
+                        float powDis = Mathf.Pow((x1 - controlPos.x), 2) - Mathf.Pow((z1 - controlPos.y), 2); ///当前点与原点的距离的平方
+                        float powr = Mathf.Pow(r, 2);
+                        if (powDis <= powr)    ///再次缩小范围if (Vector3.Distance(transform.position, new Vector3(x1, 6, z1)) < r)   //Vector3.Distance(transform.position, new Vector3(x1, 6, z1)) 
+                        {
+                            float sinkHeight; //要降低的高度
+                            float oldHeight = ter.terrainData.GetHeight(x1, z1);//输出的是介于0到1之间的高度
+                            sinkHeight = Mathf.Sqrt(powr - powDis);
+                            float[,] newHeightData = new float[1, 1] { { (oldHeight - sinkHeight) / (ter.terrainData.heightmapScale.y) } };  //新的高度值
+                            ter.terrainData.SetHeights(x1, z1, newHeightData);  //设定新的高度值
+                            isColliderTerr = false;
+                        }
                     }
                 }
             }
